a1323097c835da2d36cff67e604178ddb1869a78
Micale26 <a.micale@ileps.fr>
2025-11-20 12:02:37 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,30 +1,3 @@
-// Carrousel d'images
-let currentSlide = 0;
-const slides = document.querySelectorAll(".slide");
-
-function moveSlide(direction) {
-    currentSlide += direction;
-    if (currentSlide >= slides.length) {
-        currentSlide = 0;
-    } else if (currentSlide < 0) {
-        currentSlide = slides.length - 1;
-    }
-    showSlide(currentSlide);
-}
-
-function showSlide(index) {
-    slides.forEach((slide, i) => {
-        slide.classList.remove("active");
-        if (i === index) {
-            slide.classList.add("active");
-        }
-    });
-}
-
-window.addEventListener("load", () => {
-    showSlide(currentSlide);
-});
-
 // ScrollReveal pour apparition des sections
 const sections = document.querySelectorAll(".section");
 
